068bf7936c30645d4c42283398e70e81cd02f152
Yoann2N <yoanngervais.yg13750@gmail.com>
2025-01-20 12:02:14 +0100

    php

---
+++ test.php
@@ -0,0 +1,25 @@
+<?
+//$l = $i+$j+$k
+
+/*console.log("somme de i+j+k=",i);
+*/
+//echo "la somme = ", $l;
+
+$a = 10;
+$b = 3;
+
+/*if (a % b == 0){
+    console.log("la variable a est divisible par b ")
+    } else {
+    console.log("la variable a n'est pas divisible par b ")}
+*/
+
+if ($a % $b == 0)
+    echo $a, "la variable est divisible par", $b ;
+else 
+    echo $a, "la variable n'est pas divisible par ", $b;
+
+?>
+
+
+        
