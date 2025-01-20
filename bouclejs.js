a9c111034247017bac5e5b590a953b861b78a3df
Yoann2N <yoanngervais.yg13750@gmail.com>
2025-01-20 14:50:01 +0100

    ok

---
+++ bouclejs.js
@@ -0,0 +1,17 @@
+console.log("boucle for");
+for(let i = 0; 1 <= 10; i++);{
+    console.log(1); 
+}
+
+console.log("boucle while");
+let i = 0;
+while (i<=10){
+    console.log(i);
+    i++;
+}
+
+i = 0;
+do {
+    console.log(i);
+    i++;
+} while (1<=10);
