71d9f058aeed217f2b450122052a182df6704a9f
Yoann2N <yoanngervais.yg13750@gmail.com>
2025-01-20 12:16:21 +0100

    correction

--- test.php
+++ test.php
@@ -1,25 +1,60 @@
-<?
-//$l = $i+$j+$k
+<?php
 
-/*console.log("somme de i+j+k=",i);
+// Créer 3 variables i, j, k et leur affecter 3 valeurs numériques différentes
+/* 
+let i = 45;
+let j = 46;
+let k = 47; 
 */
-//echo "la somme = ", $l;
+$i = 45;
+$j = 46;
+$k = 47;
 
+// Additionner les 3 variables et afficher le résultat
+/*
+let l = i + j + k;
+*/
+$l = $i + $j + $k;
+
+/*
+console.log("La somme = ", l);
+*/
+echo "La somme = ", $l;
+
+/*
+console.log(i + j + k);
+*/
+
+echo $i + $j + $k;
+/*
+console.log(i, "+", j, "+", k, "=", i + j + k);
+*/
+echo "\n", $i, "+", $j, "+", $k, "=", $i + $j + $k;
+
+/*
+let a = 10, b = 3;
+*/
 $a = 10;
-$b = 3;
+$b =3;
 
-/*if (a % b == 0){
-    console.log("la variable a est divisible par b ")
-    } else {
-    console.log("la variable a n'est pas divisible par b ")}
+/*
+if (a % b == 0) 
+    console.log(a, "est divisible par", b);
+else
+    console.log(a, "n'est pas divisible par", b);
+*/
+if ($a % $b == 0) 
+    echo "\n", $a, " est divisible par ", $b;
+else
+    echo "\n", $a, " n'est pas divisible par ", $b;
+
+/*
+for(let i = 1; i < 11; i++) {
+    console.log(i);
+}
 */
 
-if ($a % $b == 0)
-    echo $a, "la variable est divisible par", $b ;
-else 
-    echo $a, "la variable n'est pas divisible par ", $b;
-
+for($i = 1; $i < 11; $i++) {
+    echo "\n", $i;
+}
 ?>
-
-
-        
